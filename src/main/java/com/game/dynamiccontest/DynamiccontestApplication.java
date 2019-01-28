0ccc5fc9d4b20707f26f309c7ddb26f976fcd0ee
rishi <rishisharma7361@gmail.com>
2019-01-29 00:37:49 +0530

    initial setup {spring-boot, JPA, Postgres, swagger, firebase}

---
+++ src/main/java/com/game/dynamiccontest/DynamiccontestApplication.java
@@ -0,0 +1,14 @@
+package com.game.dynamiccontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamiccontestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamiccontestApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/game/dynamiccontest/DynamiccontestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.game.dynamiccontest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DynamiccontestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
