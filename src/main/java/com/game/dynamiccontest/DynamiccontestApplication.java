815bc8440e796375d2bceaab7ba291101b5f7ebf
ksahu750 <ksahu750@gmail.com>
2019-01-29 12:51:07 +0530

    jobscheduler test

--- src/main/java/com/game/dynamiccontest/DynamiccontestApplication.java
+++ src/main/java/com/game/dynamiccontest/DynamiccontestApplication.java
@@ -2,13 +2,14 @@ package com.game.dynamiccontest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DynamiccontestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DynamiccontestApplication.class, args);
 	}
-
 }
 
--- src/main/java/com/game/dynamiccontest/controller/ContestController.java
+++ src/main/java/com/game/dynamiccontest/controller/ContestController.java
@@ -1,9 +1,26 @@
 package com.game.dynamiccontest.controller;
 
+import com.game.dynamiccontest.job.SendNotification;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/contest")
 public class ContestController {
+
+    @RequestMapping(value = "/createContest")
+    public void createContest(){
+        try{
+            String cron = "0/10 * 12 * * ? *";
+            JobDetail job = JobBuilder.newJob(SendNotification.class).build();
+            Trigger trigger = TriggerBuilder.newTrigger().withSchedule(CronScheduleBuilder.cronSchedule(cron)).build();
+            Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+            scheduler.start();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
+    }
 }
